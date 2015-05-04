fa16141b474ae1aa2885f725f683d27e8429e027
lepdou <ledou.zhang@dianping.com>
2015-05-04 20:14:55 +0800

    优化高优先级pipe放到queue前面

--- src/main/java/org/le/core/executor/BigPipeExecutor.java
+++ src/main/java/org/le/core/executor/BigPipeExecutor.java
@@ -1,5 +1,6 @@
 package org.le.core.executor;
 
+import org.le.anno.Weight;
 import org.le.bean.PipeProxy;
 import org.le.core.PipeExecutor;
 import org.le.core.concurrent.BigPipeWorker;
@@ -9,22 +10,31 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.*;
 
-public class BigPipeExecutor implements PipeExecutor{
+public class BigPipeExecutor implements PipeExecutor {
     private ThreadPoolExecutor executor = ThreadPoolExecutorFactoryImpl.
             newInstance().instanceOfDefaultConfig();
 
     @Override
-    public Object execute(PipeProxy pipe){
+    public Object execute(PipeProxy pipe) {
         return null;
     }
 
     @Override
-    public Map<String, Object> execute(List<PipeProxy> pipes){
+    public Map<String, Object> execute(List<PipeProxy> pipes) {
         int pipesSize = pipes.size();
         Map<String, Object> renderResult = new ConcurrentHashMap<String, Object>(pipesSize);
-        //create pipe worker
+        //create pipe worker higher weight will be push first to queue
         for (PipeProxy pipeProxy : pipes) {
-            executor.submit(new BigPipeWorker(pipeProxy, renderResult));
+            if (pipeProxy.getWeight() == Weight.HEIGHT)
+                executor.submit(new BigPipeWorker(pipeProxy, renderResult));
+        }
+        for (PipeProxy pipeProxy : pipes) {
+            if (pipeProxy.getWeight() == Weight.NORMALL)
+                executor.submit(new BigPipeWorker(pipeProxy, renderResult));
+        }
+        for (PipeProxy pipeProxy : pipes) {
+            if (pipeProxy.getWeight() == Weight.LOW)
+                executor.submit(new BigPipeWorker(pipeProxy, renderResult));
         }
         return renderResult;
     }
